e96c94aabf80331274368edfa35630c6441d01e2
ahmed15ayman7 <ahmed15ayman7ahmed2002@gmail.com>
2025-03-22 10:56:12 +0200

    updatd cloudinary

--- components/notifications/OrderNotifications.tsx
+++ components/notifications/OrderNotifications.tsx
@@ -1,31 +1,31 @@
-import React, { useEffect } from 'react';
-import { useSocket } from '@/lib/socket';
-import { toast } from 'react-toastify';
+// import React, { useEffect } from 'react';
+// import { useSocket } from '@/lib/socket';
+// import { toast } from 'react-toastify';
 
-export const OrderNotifications: React.FC = () => {
-  const socket = useSocket();
+// export const OrderNotifications: React.FC = () => {
+//   const socket = useSocket();
 
-  useEffect(() => {
-    if (!socket) return;
+//   useEffect(() => {
+//     if (!socket) return;
 
-    // استقبال إشعارات الطلبات
-    socket.on('newNotification', (notification: any) => {
-      // عرض الإشعار باستخدام react-toastify
-      toast.info(notification.message, {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        rtl: true // للغة العربية
-      });
-    });
+//     // استقبال إشعارات الطلبات
+//     socket.on('newNotification', (notification: any) => {
+//       // عرض الإشعار باستخدام react-toastify
+//       toast.info(notification.message, {
+//         position: "top-right",
+//         autoClose: 5000,
+//         hideProgressBar: false,
+//         closeOnClick: true,
+//         pauseOnHover: true,
+//         draggable: true,
+//         rtl: true // للغة العربية
+//       });
+//     });
 
-    return () => {
-      socket.off('newNotification');
-    };
-  }, [socket]);
+//     return () => {
+//       socket.off('newNotification');
+//     };
+//   }, [socket]);
 
-  return null;
-};
+//   return null;
+// };
