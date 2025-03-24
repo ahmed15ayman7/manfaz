cea5ca58bdab6ede6d056f233a470d2b82dbf7c3
ahmed15ayman7 <ahmed15ayman7ahmed2002@gmail.com>
2025-03-24 16:27:38 +0200

    update seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -118,7 +118,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
-        <meta name="google-site-verification" content="google62bbac5407f85471" />
+        <meta name="google-site-verification" content="5_m7GHPgvw_t1tOVkandnhYaqt5FVEBl8_faVIxqi4c" />
         <meta name="theme-color" content="#ffffff" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
