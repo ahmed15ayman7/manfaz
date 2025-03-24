6c2845592d335092fac3f7d6f9b7c61d270ff067
ahmed15ayman7 <ahmed15ayman7ahmed2002@gmail.com>
2025-03-24 16:42:53 +0200

    update seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,19 +61,6 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning>
       <head>
-         {/* Google Tag Manager */}
-         <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-N8D9652V');
-            `,
-          }}
-        />
-        {/* End Google Tag Manager */}
         {/* Google Tag Manager */}
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`
@@ -84,13 +71,13 @@ export default function RootLayout({
             })(window,document,'script','dataLayer','GTM-N8D9652V');
           `}
         </Script>
-        <noscript>
+        {/* <noscript>
           <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N8D9652V"
             height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
           <div style={{ display: 'none' }}>
             <img src="https://www.googletagmanager.com/ns.html?id=GTM-N8D9652V" alt="" />
           </div>
-        </noscript>
+        </noscript> */}
         {/* End Google Tag Manager */}
 
         {/* Google Analytics */}
@@ -159,6 +146,14 @@ export default function RootLayout({
       </head>
       <body className="antialiased">
         {content}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N8D9652V"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
       </body>
     </html>
   );
