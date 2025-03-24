1665cdf0805a40dfc93a9c9177cc4039ce1e9af4
ahmed15ayman7 <ahmed15ayman7ahmed2002@gmail.com>
2025-03-24 16:13:38 +0200

    update seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,14 +68,14 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-XXXXXXX');
+            })(window,document,'script','dataLayer','GTM-N8D9652V');
           `}
         </Script>
         <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N8D9652V"
             height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
           <div style={{ display: 'none' }}>
-            <img src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX" alt="" />
+            <img src="https://www.googletagmanager.com/ns.html?id=GTM-N8D9652V" alt="" />
           </div>
         </noscript>
         {/* End Google Tag Manager */}
